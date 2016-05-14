f78d9721d7bd6e1bd363b3a7a9b537abe80ed690
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2016-05-14 12:38:25 -0500

    Fix indentation

--- chat-app/app/providers/camera.js
+++ chat-app/app/providers/camera.js
@@ -4,20 +4,20 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class CameraProvider {
-	constructor(public platform: Platform) {}
+  constructor(public platform: Platform) {}
 
   takePicture(cb) {
-		this.platform.ready().then(() => {
+    this.platform.ready().then(() => {
       navigator.camera.getPicture((data) => {
-      	cb(data);
-		  }, (error) => {console.log('Error'); }, {
-		  	quality: 80,
-		  	destinationType: Camera.DestinationType.DATA_URL,
-		  	sourceType: Camera.PictureSourceType.CAMERA,
-		  	allowEdit: false,
-		  	encodingType: Camera.EncodingType.JPEG,
-		  	saveToPhotoAlbum: false
-		  });
-		});
+        cb(data);
+      }, (error) => {console.log('Error'); }, {
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        saveToPhotoAlbum: false
+      });
+    });
   }
 }
